11781564e0ccd00016ad002aaadf4af09d5edccd
Mindaugas Minelga <minelga.m@gmail.com>
2023-11-02 17:24:16 +0200

    modify main.py file

--- main.py
+++ main.py
@@ -122,36 +122,110 @@ logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='a', forma
 
 # move_to_end([1, 1, 5, 1, 7, 78], 1)
 
-def consume_gas_level(starting_level: int = 50, engine_running: bool = False) -> int:
-    if engine_running:
-        logging.debug(f"After consuming gas there is left {starting_level}")
-        return starting_level - 5
-    else:
-        logging.critical("Car can't consume gas while the engine is turned off!")
-        raise ValueError("Engine is not running, can't consume gas!")
+# def consume_gas_level(starting_level: int = 50, engine_running: bool = False) -> int:
+#     if engine_running:
+#         logging.debug(f"After consuming gas there is left {starting_level}")
+#         return starting_level - 5
+#     else:
+#         logging.critical("Car can't consume gas while the engine is turned off!")
+#         raise ValueError("Engine is not running, can't consume gas!")
     
 
-def car_toggle_switch(state: bool=False) -> bool:
-    if state == True:
-        logging.debug("Car was turned on!")
-        print("Engine is running!")
-    else:
-        logging.debug("Car was turned off!")
-        print("Engine is not running!")
-    return state
+# def car_toggle_switch(state: bool=False) -> bool:
+#     if state == True:
+#         logging.debug("Car was turned on!")
+#         print("Engine is running!")
+#     else:
+#         logging.debug("Car was turned off!")
+#         print("Engine is not running!")
+#     return state
     
 
-def drive() -> None:
-    car_state = car_toggle_switch(True)
-    gas_level = 80
-    logging.debug(f"Car was turned on with {gas_level} L of gas ")
-    while gas_level > 0:
-        message = f"Wroom wroom I have {gas_level} Litres in tank"
-        gas_level = consume_gas_level(gas_level, car_state)
-        print(message)
-        time.sleep(1)
-    print("I ran out of gass..")
-    car_state = car_toggle_switch()
+# def drive() -> None:
+#     car_state = car_toggle_switch(True)
+#     gas_level = 80
+#     logging.debug(f"Car was turned on with {gas_level} L of gas ")
+#     while gas_level > 0:
+#         message = f"Wroom wroom I have {gas_level} Litres in tank"
+#         gas_level = consume_gas_level(gas_level, car_state)
+#         print(message)
+#         time.sleep(1)
+#     print("I ran out of gass..")
+#     car_state = car_toggle_switch()
 
 
-drive()
+# drive()
+
+# import logging
+# from typing import List
+# import requests
+
+# API_KEY = "akdhflsadhflasfljssl"
+
+# logging.basicConfig(level=logging.DEBUG, filename='vmi.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+
+# def get_expenses() -> List[float]:
+#     paid = []
+#     while True:
+#         expen = input("Please enter your expenses: ")
+#         if expen.isnumeric():
+#             paid.append(float(expen))
+#             logging.debug(f"Expences entered: {expen}")
+#         else:
+#             logging.debug(f"Expences in total entered: {paid}")
+#             logging.debug(f"Sum of expences: {sum(paid)}")
+#             print(f"Sum of expences: {sum(paid)}")
+#             return sum(paid)
+
+# def get_anual_income()-> float:
+#     anual_income = input("Please enter your anual income: ")
+#     if anual_income.isnumeric():
+#         return float(anual_income)
+#     else:
+#         raise ValueError("Bad value antered")
+    
+# def calculate_profit(income: float, expenses: float, vat: int) -> float:
+#     ebita = income - expenses
+#     profit = ebita * (100 - vat) / 100
+#     logging.info(f"The profit is: {profit}")
+#     return profit
+
+# def exchange_curency(value: float, curency: str) -> float:
+#     curency_rate = get_curency_exchange_rate(curency)
+#     logging.info(f"Exchange rate for {curency}: {curency_rate}")
+#     return value * curency_rate
+
+# def get_curency_exchange_rate(name: str) -> float:
+#     result = requests.get(f"http://data.fixer.io/api/latest?access_key={API_KEY}&symbols={name}")
+#     logging.info(f"Respond from API: {result.json()}")
+#     if not result.json()["success"]:
+#         raise ValueError("Please specify curency code")    
+#     return result.json()["rates"][name]
+
+# expences = get_expenses()
+# anual_income = get_anual_income()
+# requested_curency = input("Please enter curency name USD, EU, JPY, CNY: ")
+# profit = calculate_profit(anual_income, expences, 15)
+
+
+# try:
+#     cur = exchange_curency(profit, requested_curency)
+#     print(cur)
+# except ValueError:
+#     print("Bad exchange code name")
+
+# from typing import Union
+
+
+# def my_dummy_int_func(a: Union[str, float]) -> None:
+#     try:
+#         int_value = int(a)
+#         print(int_value)
+#     except ValueError as errr:
+#         print('Value of "a" cannot be deduced to integer')
+#         print(errr.args)
+#     except TypeError as err:
+#         print('Type of "a" is incompatible; should either be a number or a string')
+#         print(err.args)
+
+# my_dummy_int_func("45h")
